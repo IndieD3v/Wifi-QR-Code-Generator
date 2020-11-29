6aaa6c959b5191822e1754af5540e645ecfc8328
Yahya Fazlani <yahyafazlani@outlook.com>
2020-11-30 00:06:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Wifi-QR-Code-Generator
+
+This is a Wifi QR Code Flask Project I created as a side project.
+
+### Prerequisites
+* Python (v3.8+) installed.
+* Git installed and configured
+* pip (v3) installed
+
+### Steps
+#### Clone the repository
+```shell
+git clone https://github.com/YahyaFazlani/Wifi-QR-Code-Maker.git
+```
+
+#### Move into the directory where you cloned it
+```shell
+cd Wifi-QR-Code-Maker
+```
+
+#### Create and activate a virtual environment
+```shell
+python -m venv env
+```
+##### MacOS/Linux
+```shell
+source "venv/bin/activate"
+```
+##### Windows
+```shell
+env\bin\activate.bat
+```
+
+#### Install requirements
+```
+pip install -r requirements.txt
+```
+
+#### Run the app
+```shell
+python app.py
+```
+## Read carefully if contributing
+***Note: If you would like to contribute please feel free to do so in any aspect like spelling errors, better styles, improved code, etc. but if you are editing the html files in the templates directory then please do minimize them as .min.html files and replace the old ones the .min files are the ones being served or if you are adding a new url please add it to the sitemap.xml. Thank you!*** 😊
---
+++ app.py
@@ -0,0 +1,51 @@
+from base64 import b64encode
+from io import BytesIO
+
+from wifi_qrcode_generator import wifi_qrcode
+from flask import Flask, abort, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return render_template("Pages/index.min.html")
+
+
+@app.route('/result', methods=["POST"])
+def result():
+  if 'ssid' in request.form and 'password' in request.form and 'encryption' in request.form:
+    ssid = request.form.get("ssid")
+    password = request.form.get("password")
+    encryption = request.form.get("encryption")
+  else:
+    abort(404)
+  hidden = request.form.get("hidden", default=False)
+  if hidden == "on":
+    hidden = True
+
+  qr_code = wifi_qrcode(
+      ssid, hidden, encryption, password
+  )
+
+  output = BytesIO()
+  qr_code.save(output, "PNG")
+  image = output.getvalue()
+  output.close()
+  image = str(b64encode(image))[2:-1]
+  return render_template("Pages/result.min.html", image=image)
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+  return render_template("sitemap.xml")
+
+
+@app.route("/google5c02155bdaa39045.html")
+def google_verification():
+  return render_template("google5c02155bdaa39045.html")
+
+
+if __name__ == '__main__':
+  app.env = "development"
+  app.run(debug=True)
